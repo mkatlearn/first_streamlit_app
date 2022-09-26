52f4cf6d649424db86f9427065da7698f1854598
mkatlearn <114352350+mkatlearn@users.noreply.github.com>
2022-09-25 19:11:09 -0600

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Dinner')
+streamlit.title('My Kids New Healthy Breakfast')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
