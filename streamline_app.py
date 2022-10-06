eb2401cae23aef15b867e55d909743cdfa36ee0d
mkatlearn <114352350+mkatlearn@users.noreply.github.com>
2022-10-05 21:07:34 -0600

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -27,16 +27,25 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-
+try:
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+if not fruit_choice:
+  streamlit.error ("PLease select a fruit to get nutrion information.")
+else:
 
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  streamlit.dataframe(my_data_rows)
+except URLError as e:
+  streamlit.error()
+  
+streamlit.write('The user entered ', fruit_choice)
 streamlit.text(fruityvice_response)
+#import requests
+
 
 # write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 #dont run anything while we troubleshot
@@ -48,7 +57,7 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+
 
 #Allow end users to add a fruit
 add_my_fruit = streamlit.text_input("What fruit would you like to add?")
