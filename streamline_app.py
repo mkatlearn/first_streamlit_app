b1435acea828a6fb66fffc7b3be8191e68dd3a2f
mkatlearn <114352350+mkatlearn@users.noreply.github.com>
2022-10-05 20:55:48 -0600

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -1,4 +1,9 @@
 import streamlit
+import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Kids New Healthy Breakfast')
 
@@ -10,7 +15,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🍓 Build Your Own Fruit Smoothie 🥝🍇')
 
 
-import pandas
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -26,7 +31,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
@@ -34,7 +39,10 @@ streamlit.text(fruityvice_response)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
-import snowflake.connector
+#dont run anything while we troubleshot
+streamlit.stop()
+
+#import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
@@ -44,7 +52,7 @@ streamlit.dataframe(my_data_rows)
 
 #Allow end users to add a fruit
 add_my_fruit = streamlit.text_input("What fruit would you like to add?")
-streamlit.write('TThank you for adding ', add_my_fruit)
+streamlit.write('Thank you for adding ', add_my_fruit)
 
 my_cur.execute("Insert into fruit_Load_list values('from streamlit')")
 
